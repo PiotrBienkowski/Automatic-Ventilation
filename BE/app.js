f17893e6a03ca906ed354079e78291789e17cb80
Piotr Bieńkowski <120416913+PiotrBienkowski@users.noreply.github.com>
2024-06-21 22:01:38 +0200

    Added endpoint

--- BE/app.js
+++ BE/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const db = require('./models');
+const userController = require('./controllers/userController');
+const temperatureController = require('./controllers/temperatureController');
+
 
 const app = express();
 const port = 3002;
@@ -31,13 +34,22 @@ app.get('/users', async (req, res) => {
 
 app.get('/temperatures', async (req, res) => {
     try {
-        const temperatures = await db.Temperature.findAll();
+        const temperatures = await temperatureController.getAllTemperatures();
         res.json(temperatures);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 });
-  
+
+app.post('/temperatures', async (req, res) => {
+    try {
+        const { username, password, inside, outside } = req.body;
+        const temperature = await temperatureController.addTemperature(username, password, inside, outside);
+        res.status(201).json(temperature);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
 app.listen(port, () => {
     console.log(`API działa na http://localhost:${port}`);
